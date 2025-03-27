a7798a82cc664946787b15d5a0e8e607672a4147
Ethan Silverthorne <emsilver987@gmail.com>
2025-03-27 15:53:15 -0400

    Created table class that contains money and cards for table

---
+++ src/main/java/org/example/poker/Table.java
@@ -0,0 +1,45 @@
+package org.example.poker;
+
+public class Table {
+    Card cardOne;
+    Card cardTwo;
+    Card cardThree;
+    Card cardFour;
+    Card cardFive;
+    int money;
+
+    public Table(){
+        Deck sharedDeck = Deck.getInstance();
+        this.cardOne = sharedDeck.getRandomCard();
+        this.cardTwo = sharedDeck.getRandomCard();
+        this.cardThree = sharedDeck.getRandomCard();
+        this.cardFour = sharedDeck.getRandomCard();
+        this.cardFive = sharedDeck.getRandomCard();
+        this.money = money;
+    }
+
+    public Card getCard(int index) throws Exception {
+        if (index == 1){
+            return cardOne;
+        }
+        else if (index == 2){
+            return cardTwo;
+        }
+        else if (index == 3){
+            return cardThree;
+        }
+        else if (index == 4){
+            return cardFour;
+        }
+        else if (index == 5){
+            return cardFive;
+        }
+        else {
+            throw new Exception("Something went wrong");
+        }
+    }
+    public int getMoney() {
+        return money;
+    }
+
+}
