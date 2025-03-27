836b2fabf202dfffb51a2e912a0015c9aa56feea
Ethan Silverthorne <emsilver987@gmail.com>
2025-03-27 09:56:57 -0400

    Starting point for messages class

---
+++ src/main/java/org/example/poker/Messages.java
@@ -0,0 +1,38 @@
+package org.example.poker;
+
+public class Messages {
+    private final String welcomePokerMessage = "Welcome to poker";
+    private final String[] moneyMessage = {"You have ", " dollars"};
+    private final String preFlopMessage = "Dealer deals cards to you and 5 other players";
+    private final String flopMessage = "Flop - Cards Dealt: ";
+    private final String turnMessage = "Turn - Cards Dealt: ";
+    private final String riverMessage = "River - Cards Dealt: ";
+    private final String underTheGunChoieMessage = "Call(1), Raise(2), Fold(3)";
+    private final String choieMessage = "Check(1), Raise(2), Fold(3)";
+
+    public String getWelcomePokerMessage() {
+        return welcomePokerMessage;
+    }
+    public String getMoneyMessage() {
+        return moneyMessage[0] + moneyMessage[1];
+        // Should have a static instance of money that could return that. Don't want to pass argument
+    }
+    public String getPreFlopMessage() {
+        return preFlopMessage;
+    }
+    public String getFlopMessage(Card cardOne, Card cardTwo, Card cardThree) {
+        return flopMessage + cardOne + cardTwo + cardThree;
+    }
+    public String getTurnMessage(Card cardFour) {
+        return turnMessage + cardFour;
+    }
+    public String getRiverMessage(Card cardFive) {
+        return riverMessage + cardFive;
+    }
+    public String getUnderTheGunChoieMessage(){
+        return underTheGunChoieMessage;
+    }
+    public String getChoieMessage() {
+        return choieMessage;
+    }
+}
