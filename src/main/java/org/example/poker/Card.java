ba2c0fdea1a774e4759658e97499fd09c8669110
Ethan Silverthorne <emsilver987@gmail.com>
2025-04-10 18:51:46 -0400

    Add new method to getRankIndex() will want to refactor - is terrible

--- src/main/java/org/example/poker/Card.java
+++ src/main/java/org/example/poker/Card.java
@@ -1,18 +1,19 @@
 package org.example.poker;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.Arrays;
 import java.util.Objects;
-import java.util.Random;
-import static org.example.poker.Deck.getInstance;
+
 
 public class Card {
         private String suit;
         private String rank;
+        private String rankIndex;
 
         public Card(String suit, String rank) {
             this.suit = suit;
             this.rank = rank;
+            this.rankIndex = rank;
             if (Objects.equals(rank, "A")){
                 this.rank = "Ace";
             }
@@ -33,6 +34,50 @@ public class Card {
 
         public String getRank() {
             return rank;
+        }
+
+        // This is terrible practice but I will leave while i work on other things
+        public int getRankIndex() {
+            if (Objects.equals(rank, "A")){
+                return 14;
+            }
+            else if (Objects.equals(rank, "K")){
+                return 13;
+            }
+            else if (Objects.equals(rank, "Q")){
+                return 12;
+            }
+            else if (Objects.equals(rank, "J")){
+                return 11;
+            }
+            else if (Objects.equals(rank, "10")){
+                return 10;
+            }
+            else if (Objects.equals(rank, "9")){
+                return 9;
+            }
+            else if (Objects.equals(rank, "8")){
+                return 8;
+            }
+            else if (Objects.equals(rank, "7")){
+                return 7;
+            }
+            else if (Objects.equals(rank, "6")){
+                return 6;
+            }
+            else if (Objects.equals(rank, "5")){
+                return 5;
+            }
+            else if (Objects.equals(rank, "4")){
+                return 4;
+            }
+            else if (Objects.equals(rank, "3")){
+                return 3;
+            }
+            else if (Objects.equals(rank, "2")){
+                return 22;
+            }
+            return 100000;
         }
 
         @Override
---
+++ src/main/java/org/example/poker/logic/HandRankType.java
@@ -0,0 +1,4 @@
+package org.example.poker.logic;
+
+public enum HandRankType {
+}
