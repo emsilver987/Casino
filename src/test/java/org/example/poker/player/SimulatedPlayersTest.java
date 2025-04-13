26d4ceb061083db050ba1f4aa91f1d936667f966
Ethan Silverthorne <emsilver987@gmail.com>
2025-04-12 22:09:05 -0400

    Ensure tests worked when ran together

--- src/test/java/org/example/poker/player/SimulatedPlayersTest.java
+++ src/test/java/org/example/poker/player/SimulatedPlayersTest.java
@@ -1,12 +1,19 @@
 package org.example.poker.player;
 
+import org.example.poker.Deck;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.example.poker.player.SimulatedPlayers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimulatedPlayersTest {
+
+    @BeforeEach
+    void setUp() {
+        Deck.resetInstance();
+    }
 
     @Test
     public void getAllPlayersTest(){
